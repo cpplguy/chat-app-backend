b7ef29827f3e496c8395157e8298430a91e65198
Ramon Guo <ramonguo@Ramons-MacBook-Pro.local>
2025-08-18 18:39:46 -0400

    fixed something

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 router.get("/", authenticate, (req, res) => {
     const token = req.token;
-    console.log("Authenticated user: ", req.user.email); 
+    console.log("Authenticated user: ", req.user.email, "token: ", token); 
     res.status(200).json({message: "Authenticated", user: req.user, token: token});
 });
 module.exports = router;
