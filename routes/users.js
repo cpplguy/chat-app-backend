bf5f8f07d7523e8cc1eda2e5376b1ab404b28f7d
Ramon Guo <ramonguo@Ramons-MacBook-Pro.local>
2025-10-27 20:17:58 -0400

    cookies update

--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.delete("/logout", (req, res) => {
     res.clearCookie("auth", {
       httpOnly: true,
       secure: process.env.STATUS === "development" ? false : true,
-      sameSite: process.env.STATUS === "development" ? "Lax" : "none",
+      sameSite: /*process.env.STATUS === "development" ? "Lax" :*/ "none",
       path: "/",
     });
     return res.sendStatus(200);
@@ -138,7 +138,7 @@ router.post("/login", async (req, res) => {
     .cookie("auth", token, {
       httpOnly: true,
       secure: process.env.STATUS === "development" ? false : true,
-      sameSite: process.env.STATUS === "development" ? "Lax" : "none",
+      sameSite: /* process.env.STATUS === "development" ? "Lax" :*/ "none",
       maxAge: 1000 * 60 * 60 * 100, // 100 hours
       path: "/",
     })
