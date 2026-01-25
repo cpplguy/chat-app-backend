fe55901ed57a93064221cca3474d63a76e9e0abf
Ramon Guo <ramonguo@Ramons-MacBook-Pro.local>
2026-01-24 22:36:37 -0500

    added function

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,17 @@
 const authenticate = require("./authenticate.js");
 const bannedAuthenticate = require("./banned.js");
 const express = require("express");
+const bannedIps = require("../database/bannedips.js");
 const router = express.Router();
-router.get("/", bannedAuthenticate, authenticate, (req, res) => {
+router.get("/", bannedAuthenticate, authenticate, async (req, res) => {
   const token = req.token;
   console.log("Authenticated user/user logged in: ", req.user);
+  //delete this
+  const banned = new bannedIps({
+    ip: req.ip,
+    banned:false
+  })
+  await banned.save()
   res
     .status(200)
     .json({ message: "Authenticated", user: req.user, token: token });
